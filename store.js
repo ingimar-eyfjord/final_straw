023aa34e440ea0117abe9e70e01a0fcc313c31ce
Ingimar <ingimareys93@gmail.com>
2019-12-09 14:36:21 +0100

    Linked to server, carausel is finsihed and Ive added to store, added navbar into it as well

--- store.js
+++ store.js
@@ -1,43 +1,20 @@
 // JavaScript Document
-window.addEventListener("DOMContentLoaded", getData);
+
 window.addEventListener("DOMContentLoaded", getfetured);
-function getData() {
-	fetch("https://iesdesigner.eu/school-folder/2-semester/final-straw/wordpress/wp-json/wp/v2/work?_embed").then(res => res.json()).then(handleData)
-}
-function handleData(myData) {
-	myData.forEach(showPost)
-}
-function showPost(post) {
-	const template = document.querySelector(".template").content;
-	const postCopy = template.cloneNode(true);
-	
-	const h1 = postCopy.querySelector("h1");
-	h1.textContent = post.title.rendered;
-	const imgPath = post.cover.guid;
-	const img = postCopy.querySelector("img");
-	img.setAttribute("src", imgPath)
-	img.setAttribute("alt", "Poster of the movie " + post.title.rendered);
-	const bodyinfo = postCopy.querySelector(".bodyinfo");
-	bodyinfo.innerHTML = post.content.rendered;
-	
-	var a = postCopy.querySelector(".seemore");
-	a.href = `work_single.html?id=${post.id}`;
-	
- document.querySelector("#work").appendChild(postCopy)}
 
 function getfetured() {
 	fetch("https://iesdesigner.eu/school-folder/2-semester/final-straw/wordpress/wp-json/wp/v2/work?_embed").then(res => res.json()).then(featuredyes).then(() => {
 	
-	const track = document.querySelector(".carousel_track");
+	const track = document.querySelector(".storecarousel_track");
 	const slides = Array.from(track.children);
-	const lastslide = slides.length - 4;
+	const lastslide = slides.length - 3;
 		
 		
-	const nextButton = document.querySelector(".carousel_button--right");
-	const prevButton = document.querySelector(".carousel_button--left");
+	const nextButton = document.querySelector(".storecarousel_button--right");
+	const prevButton = document.querySelector(".storecarousel_button--left");
 		
 		
-	const dotNav = document.querySelector(".caroselnav");
+	const dotNav = document.querySelector(".storecaroselnav");
 	const dots = Array.from(dotNav.children);
 		
 	slides[0].classList.add("currentSlide");
@@ -46,8 +23,8 @@ function getfetured() {
 	console.log(slides);
 		
 		function removeprevbutton(){
-		if (document.querySelector(".carousel-slide").classList.contains("currentSlide")){
-		document.querySelector(".carousel_button--left").style.display = "none";}	}
+		if (document.querySelector(".storecarousel-slide").classList.contains("currentSlide")){
+		document.querySelector(".storecarousel_button--left").style.display = "none";}	}
 		removeprevbutton()
 
 	nextButton.addEventListener("click", e =>{
@@ -56,17 +33,17 @@ function getfetured() {
 	const amounttomove = NextSlide.style.transform.slideWidth;
 	const lastSlide = track.querySelector(".lastSlide")
 	
-	document.querySelector(".carousel_button--left").style.display = "block";
+	document.querySelector(".storecarousel_button--left").style.display = "block";
 	
 	
-	document.querySelectorAll(".carousel-slide").forEach(e => {
+	document.querySelectorAll(".storecarousel-slide").forEach(e => {
 
 				if (e.classList.contains("currentSlide") && e.classList.contains("lastSlide")) {
 					console.log("object");
-					document.querySelector(".carousel_button--right").style.display = "none";
+					document.querySelector(".storecarousel_button--right").style.display = "none";
 				}
 			})
-			track.style.transform += "translateX(-33vw)";
+			track.style.transform += "translateX(-50vw)";
 			currentSlide.classList.remove("currentSlide");
 			NextSlide.classList.add("currentSlide");
 //			document.querySelector(".carousel_button--right").style.display = "block";
@@ -79,10 +56,10 @@ function getfetured() {
 	const prevSlide = currentSlide.previousElementSibling;
 	const amounttomove = prevSlide.style.transform.slideWidth;
 	
-	track.style.transform += "translateX(33vw)";
+	track.style.transform += "translateX(50vw)";
 	currentSlide.classList.remove("currentSlide");
 	prevSlide.classList.add("currentSlide");
-	document.querySelector(".carousel_button--right").style.display = "block";
+	document.querySelector(".storecarousel_button--right").style.display = "block";
 	removeprevbutton()
 	})			
 })
@@ -93,7 +70,7 @@ function featuredyes(featdata) {
 	featdata.forEach(showfeatured)
 }
 function showfeatured(featuredata){
-	const template = document.querySelector(".featuredtemplate").content;
+	const template = document.querySelector(".storefeaturedtemplate").content;
 	const postCopy = template.cloneNode(true);
 	const carnavcopy = postCopy.querySelector("button");
 	
@@ -110,13 +87,13 @@ function showfeatured(featuredata){
 	
 	function appendcarnav(){
 		if (featuredata.featured == 1){
-		document.querySelector(".caroselnav").appendChild(carnavcopy);
+		document.querySelector(".storecaroselnav").appendChild(carnavcopy);
 	}}
 	appendcarnav()
 	function appendfeatured(){
 	
 	if (featuredata.featured == 1)
-	{document.querySelector(".carousel_track").appendChild(postCopy);
+	{document.querySelector(".storecarousel_track").appendChild(postCopy);
 	}}
 	appendfeatured()
 }
---
+++ work_single.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+window.addEventListener("DOMContentLoaded", getData);
+function getData() {
+	fetch("https://iesdesigner.eu/school-folder/2-semester/final-straw/wordpress/wp-json/wp/v2/work?_embed").then(res => res.json()).then(handleData)
+}
+function handleData(myData) {
+	myData.forEach(showPost)
+}
+function showPost(post) {
+	const template = document.querySelector(".template").content;
+	const postCopy = template.cloneNode(true);
+	
+	const h1 = postCopy.querySelector("h1");
+	h1.textContent = post.title.rendered;
+	const imgPath = post.cover.guid;
+	const img = postCopy.querySelector("img");
+	img.setAttribute("src", imgPath)
+	img.setAttribute("alt", "Poster of the movie " + post.title.rendered);
+	const bodyinfo = postCopy.querySelector(".bodyinfo");
+	bodyinfo.innerHTML = post.content.rendered;
+	
+	var url_string = (window.location.href).toLowerCase();
+	var url = new URL(url_string);
+	var id = url.searchParams.get("id");
+
+	function appendWork() {
+		if (post.id == id) document.querySelector("#work").appendChild(postCopy);
+	}
+	appendWork()
+}
