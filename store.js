80ae84ab7485e9d4b47f585ea28650f093f690ea
Ingimar Eyfjord Smarason <ingimareys93@gmail.com>
2019-12-10 10:19:04 +0100

    Final commit before

--- store.js
+++ store.js
@@ -2,10 +2,8 @@
 window.addEventListener("DOMContentLoaded", getData);
 window.addEventListener("DOMContentLoaded", getfetured);
 window.addEventListener("DOMContentLoaded", getmedium);
-
-
 function getData() {
-	fetch("https://iesdesigner.eu/school-folder/2-semester/final-straw/wordpress/wp-json/wp/v2/work?_embed").then(res => res.json()).then(handleData)
+	fetch("https://iesdesigner.eu/school-folder/2-semester/final-straw/wordpress/wp-json/wp/v2/store?_embed").then(res => res.json()).then(handleData)
 }
 function handleData(myData) {
 	myData.forEach(showPost)
@@ -20,10 +18,9 @@ function showPost(post) {
 	const img = postCopy.querySelector("img");
 	img.setAttribute("src", imgPath)
 	img.setAttribute("alt", "Poster of the movie " + post.title.rendered);
-//	const bodyinfo = postCopy.querySelector(".bodyinfo");
-//	bodyinfo.innerHTML = post.content.rendered;
+	const bodyinfo = postCopy.querySelector(".bodyinfo");
+	bodyinfo.innerHTML = post.content.rendered;
 	
-//	console.log(post.medium[0].post_title);
 	post.medium.forEach(e =>{
 	const infowrapper = postCopy.querySelector(".seemore");
 	const classadd = e.post_title;
@@ -35,41 +32,34 @@ function showPost(post) {
 	infowrapper.setAttribute("value", classadd)
 	})
 	
-	
-	const yearmade = postCopy.querySelector(".workyearmade");
-	yearmade.textContent = post.year_made;
-	const dimmentions = postCopy.querySelector(".dimentions");
-	dimmentions.textContent = post.dimentions;
-	
 	var a = postCopy.querySelector(".seemore");
-	a.href = `work_single.html?id=${post.id}`;
+	a.href = `store_single.html?id=${post.id}`;
 	
- document.querySelector("#work").appendChild(postCopy)}
+ document.querySelector("#store").appendChild(postCopy)}
 
-function getfetured(post) {
-	const template = document.querySelector(".template").content;
-	const postCopy = template.cloneNode(true);
-	fetch("https://iesdesigner.eu/school-folder/2-semester/final-straw/wordpress/wp-json/wp/v2/work?_embed").then(res => res.json()).then(featuredyes).then(() => {
+function getfetured() {
+	fetch("https://iesdesigner.eu/school-folder/2-semester/final-straw/wordpress/wp-json/wp/v2/store?_embed").then(res => res.json()).then(featuredyes).then(() => {
 	
-	const track = document.querySelector(".carousel_track");
+	const track = document.querySelector(".storecarousel_track");
 	const slides = Array.from(track.children);
-	const lastslide = slides.length - 4;
+	const lastslide = slides.length - 3;
 		
 		
-	const nextButton = document.querySelector(".carousel_button--right");
-	const prevButton = document.querySelector(".carousel_button--left");
+	const nextButton = document.querySelector(".storecarousel_button--right");
+	const prevButton = document.querySelector(".storecarousel_button--left");
 		
-	const dotNav = document.querySelector(".caroselnav");
+		
+	const dotNav = document.querySelector(".storecaroselnav");
 	const dots = Array.from(dotNav.children);
 		
 	slides[0].classList.add("currentSlide");
 	slides[lastslide].classList.add("lastSlide");
 	const slideWidth = slides[0].getBoundingClientRect().width;	
-//	console.log(slides);
+	console.log(slides);
 		
 		function removeprevbutton(){
-		if (document.querySelector(".carousel-slide").classList.contains("currentSlide")){
-		document.querySelector(".carousel_button--left").style.display = "none";}	}
+		if (document.querySelector(".storecarousel-slide").classList.contains("currentSlide")){
+		document.querySelector(".storecarousel_button--left").style.display = "none";}	}
 		removeprevbutton()
 
 	nextButton.addEventListener("click", e =>{
@@ -78,20 +68,19 @@ function getfetured(post) {
 	const amounttomove = NextSlide.style.transform.slideWidth;
 	const lastSlide = track.querySelector(".lastSlide")
 	
-	document.querySelector(".carousel_button--left").style.display = "block";
+	document.querySelector(".storecarousel_button--left").style.display = "block";
 	
 	
-	document.querySelectorAll(".carousel-slide").forEach(e => {
+	document.querySelectorAll(".storecarousel-slide").forEach(e => {
 
 				if (e.classList.contains("currentSlide") && e.classList.contains("lastSlide")) {
 					console.log("object");
-					document.querySelector(".carousel_button--right").style.display = "none";
+					document.querySelector(".storecarousel_button--right").style.display = "none";
 				}
 			})
-			track.style.transform += "translateX(-33vw)";
+			track.style.transform += "translateX(-50vw)";
 			currentSlide.classList.remove("currentSlide");
 			NextSlide.classList.add("currentSlide");
-//			document.querySelector(".carousel_button--right").style.display = "block";
 		removeprevbutton()
 	})
 	
@@ -101,10 +90,10 @@ function getfetured(post) {
 	const prevSlide = currentSlide.previousElementSibling;
 	const amounttomove = prevSlide.style.transform.slideWidth;
 	
-	track.style.transform += "translateX(33vw)";
+	track.style.transform += "translateX(50vw)";
 	currentSlide.classList.remove("currentSlide");
 	prevSlide.classList.add("currentSlide");
-	document.querySelector(".carousel_button--right").style.display = "block";
+	document.querySelector(".storecarousel_button--right").style.display = "block";
 	removeprevbutton()
 	})			
 })
@@ -115,34 +104,28 @@ function featuredyes(featdata) {
 	featdata.forEach(showfeatured)
 }
 function showfeatured(featuredata){
-	const template = document.querySelector(".featuredtemplate").content;
+	const template = document.querySelector(".storefeaturedtemplate").content;
 	const postCopy = template.cloneNode(true);
 	const carnavcopy = postCopy.querySelector("button");
 	
 	const imgPath = featuredata.cover.guid;
 	const img = postCopy.querySelector("img");
 	
-		var a = postCopy.querySelector(".carousel-slide");
-		a.href = `work_single.html?id=${featuredata.id}`;
-
-	
 	img.setAttribute("src", imgPath)
 	img.setAttribute("alt", "Poster of the movie " + featuredata.title.rendered);
 	
-//	const h1 = postCopy.querySelector("h1");
-//	h1.textContent = featuredata.title.rendered;
-
-//	console.log(featuredata.querySelector(".carousel-slide").classList.contains("lastslide"));
+		var a = postCopy.querySelector(".storecarousel-slide");
+		a.href = `store_single.html?id=${featuredata.id}`;
 	
 	function appendcarnav(){
 		if (featuredata.featured == 1){
-		document.querySelector(".caroselnav").appendChild(carnavcopy);
+		document.querySelector(".storecaroselnav").appendChild(carnavcopy);
 	}}
 	appendcarnav()
 	function appendfeatured(){
 	
 	if (featuredata.featured == 1)
-	{document.querySelector(".carousel_track").appendChild(postCopy);
+	{document.querySelector(".storecarousel_track").appendChild(postCopy);
 	}}
 	appendfeatured()
 }
@@ -158,13 +141,13 @@ var currentScrollPos = window.pageYOffset;
   prevScrollpos = currentScrollPos;
 }
 
-
 function getmedium() {
 	fetch("https://iesdesigner.eu/school-folder/2-semester/final-straw/wordpress/wp-json/wp/v2/medium?_embed").then(res => res.json()).then(handlemediumData)
 }
 function handlemediumData(myData) {
 	myData.forEach(showmediumPost)
 }
+
 function showmediumPost(post) {
 	const template = document.querySelector(".mediumnactemplate").content;
 	const postCopy = template.cloneNode(true);
