284bdd51cfca7808edcc44e642af36404653cc0d
Ingimar <ingimareys93@gmail.com>
2019-12-13 12:15:51 +0100

    Added 3 works on homepage, need's styling

---
+++ index.js
@@ -0,0 +1,44 @@
+// JavaScript Document
+window.addEventListener("DOMContentLoaded", getData);
+
+function getData() {
+	fetch("https://iesdesigner.eu/school-folder/2-semester/final-straw/wordpress/wp-json/wp/v2/work?per_page=3").then(res => res.json()).then(handleData)
+}
+function handleData(myData) {
+	myData.forEach(showPost)
+}
+function showPost(post) {
+	const template = document.querySelector(".template").content;
+	const postCopy = template.cloneNode(true);
+	
+	const h1 = postCopy.querySelector("h1");
+	h1.textContent = post.title.rendered;
+	const imgPath = post.cover.guid;
+	const img = postCopy.querySelector("img");
+	img.setAttribute("src", imgPath)
+	img.setAttribute("alt", "Poster of the movie " + post.title.rendered);
+//	const bodyinfo = postCopy.querySelector(".bodyinfo");
+//	bodyinfo.innerHTML = post.content.rendered;
+	
+//	console.log(post.medium[0].post_title);
+	post.medium.forEach(e =>{
+	const infowrapper = postCopy.querySelector(".seemore");
+	const classadd = e.post_title;
+	
+//	infowrapper.classList.add(`${classadd}`);
+////		console.log(classadd);console.log(infowrapper);
+////	infowrapper.classList.add("$[post.medium.title.rendered]");
+//		console.log(infowrapper);
+//	infowrapper.setAttribute("value", classadd)
+//	})
+	
+	
+	const yearmade = postCopy.querySelector(".workyearmade");
+	yearmade.textContent = post.year_made;
+	const dimmentions = postCopy.querySelector(".dimentions");
+	dimmentions.textContent = post.dimentions;
+	
+	var a = postCopy.querySelector(".seemore");
+	a.href = `work_single.html?id=${post.id}`;
+	
+ document.querySelector("#work").appendChild(postCopy)})}
